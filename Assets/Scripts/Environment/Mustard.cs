0fbdde13895ac5e00e5e6e424c7a5696545472eb
Ceogear <mustafaenesbilgic@gmail.com>
2020-04-12 02:08:40 +0300

    Collectables, Mustard, Heart, Score

---
+++ Assets/Scripts/Environment/Mustard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mustard : Collectables
+{
+    [SerializeField] protected UnityEngine.UI.Text myText;
+    int score;
+
+    void Start()
+    {
+        collectableSprite = GetComponent<SpriteRenderer>(); 
+        score = System.Int32.Parse(myText.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Collected())
+        {
+            UpdateScore();
+            Suicide();
+        }
+    } 
+
+    void UpdateScore()
+    {
+        ++score;
+        myText.text = score.ToString();
+    }
+}
